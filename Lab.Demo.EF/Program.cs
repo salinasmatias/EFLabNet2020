31f032bf22eb4d89535b10aca13747cbacd69940
Matias <salinasmatias1990@gmail.com>
2020-11-01 03:21:31 -0300

    Agregadas Logic de Customer y Shipper

--- Lab.Demo.EF/Program.cs
+++ Lab.Demo.EF/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Lab.Demo.Logic;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,35 @@ namespace Lab.Demo.EF
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Test");
+            ShippersLogic shippersLogic = new ShippersLogic();
+            var shippersList = shippersLogic.GetShippersList();
+            CustomersLogic customersLogic = new CustomersLogic();
+            var customersList = customersLogic.GetCustomersList();
+
+            foreach (var shipper in shippersList) 
+            {
+                Console.WriteLine("Shipper ID: {0}\nCompany name: {1}\nPhone:{2}\n", shipper.ShipperID, shipper.CompanyName, shipper.Phone);
+            }
+
+            foreach (var customer in customersList) 
+            {
+                Console.WriteLine("Customer ID: {0}\nContact Name: {1}\nContactTitle:{2}\nAddreess: {3}\nCity: {4}\n",
+                    customer.CustomerID, customer.ContactName, customer.ContactTitle, customer.Address, customer.City);
+            }
+
+            try
+            {
+                var shipper = shippersLogic.GetShipperByID(4);
+                Console.WriteLine("Shipper ID: {0}\nCompany name: {1}\nPhone:{2}\n", shipper.ShipperID, shipper.CompanyName, shipper.Phone);
+            }
+            catch(InvalidOperationException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
             Console.ReadKey();
         }
     }
